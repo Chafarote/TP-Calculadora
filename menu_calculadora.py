f6ec005c782594bdde80a9f16079cd17437f785b
Gabriel Gomez <98gmgomez@gmail.com>
2024-05-23 19:10:41 -0300

    Correccion de algunos posibles errores

--- menu_calculadora.py
+++ menu_calculadora.py
@@ -8,23 +8,36 @@ resultado = ""
 while True:
     match menu(operando_a, operando_b):
         case "1":
-            operando_a = operando()
+            if operando_a == "x":
+                operando_a = operando()
+            else:
+                print("Ya se ingreso el primer operando")
         case "2":
-            operando_b = operando()
+            if operando_b == "y":
+                operando_b = operando()
+            else:
+                print("Ya se ingreso el segundo operando")
         case "3":
-            operacion = menu_calcular(operando_a, operando_b)
-            if operacion == "a":
-                resultado = suma(operando_a, operando_b)
-            elif operacion == "b":
-                resultado = resta(operando_a, operando_b)
-            elif operacion == "c":
-                resultado = division(operando_a, operando_b)
-            elif operacion == "d":
-                resultado = multiplicacion(operando_a, operando_b)
-            elif operacion == "e":
-                resultado = {"resultado_1":factorial(operando_a), "resultado_2":factorial(operando_b)}
+            if operando_a == "x" or operando_b == "y":
+                print("No se ingresaron operandos")
+            else:
+                operacion = menu_calcular(operando_a, operando_b)
+                if operacion == "a":
+                    resultado = suma(operando_a, operando_b)
+                elif operacion == "b":
+                    resultado = resta(operando_a, operando_b)
+                elif operacion == "c":
+                    resultado = division(operando_a, operando_b)
+                elif operacion == "d":
+                    resultado = multiplicacion(operando_a, operando_b)
+                elif operacion == "e":
+                    resultado = {"resultado_1":factorial(operando_a), "resultado_2":factorial(operando_b)}
         case "4":
             informar_resultado(operacion, operando_a, operando_b, resultado)
+            operando_a = "x"
+            operando_b = "y"
         case "5":
             break
+        case _:
+            print("No se ingreso una opcion valida")
     pausar_pantalla()
